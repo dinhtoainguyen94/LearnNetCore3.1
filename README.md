632a27e599d654ea985f12ed94a945812374ef1f
ToaiND <dinhtoai250694@gmail.com>
2020-12-18 16:27:16 +0700

    commit lesson7 18/12/2020

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# LearnNetCore3.1
+# Learning .NET Core 3.1
+## Technologies
+- ASP.NET Core 3.1
+- Entity Framework core 3.1
+## Install Package
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.EntityFrameworkCore.Tools
---
+++ eShopSolution/eShopSolution.Data/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Price { get; set; }
+        public string OriginalPrice { get; set; }
+        public string Stock { get; set; }
+        public string VewCount { get; set; }
+        public string DateCreated { get; set; }
+        public string SeoAlias { get; set; }
+
+    }
+}
