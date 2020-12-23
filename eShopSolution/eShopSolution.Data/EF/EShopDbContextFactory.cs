5f5ab8663757e81585379acc0868b175fa698929
ToaiND <dinhtoai250694@gmail.com>
2020-12-23 23:16:10 +0700

    Commit lesson9 23/12/2020

---
+++ eShopSolution/eShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        //Cấu hình đọc file json kết nối database 
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            // Config đọc file setting.json
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsetting.json").Build();
+           // Đọc chuỗi kết nối trong json file (setting.json)
+            var connectionString = configuration.GetConnectionString("EShopsolutionDatabase");
+            
+            //Kết nối với database qua connectstring
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EShopDbContext(optionsBuilder.Options);
+        }
+
+    }
+}
